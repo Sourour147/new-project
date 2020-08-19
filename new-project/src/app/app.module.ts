9091d9240f25f1990ed238221bf2ea49083a085e
sourour147 <chalbisourour23@gmail.com>
2020-08-19 13:38:45 +0100

    Added my project

---
+++ new-project/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatCardModule } from '@angular/material/card';
+import { environment } from '../environments/environment';
+import { FileService } from './service/file.service';
+import { FileExplorerModule } from './file-explorer/file-explorer.module';
+
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, FileExplorerModule, FlexLayoutModule,
+    HttpClientModule ,  MatCardModule],
+  providers: [FileService],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule {}
